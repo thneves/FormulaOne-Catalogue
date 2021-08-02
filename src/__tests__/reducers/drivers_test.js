3353ff290ffe819215cee5ef7c784e1ca1f9f18c
thneves <tsneves11@gmail.com>
2021-08-02 18:32:55 -0300

    Create tests for drivers ranking reducer

---
+++ src/__tests__/reducers/drivers_test.js
@@ -0,0 +1,31 @@
+import drivers, { initialState} from '../../redux/reducers/drivers';
+
+describe('Drivers Ranking Reducer', () => {
+  it ('should return the initial state', () => {
+    expect(drivers(undefined, {})).toMatchSnapshot();
+  })
+
+  it ('should handle Fetch Drivers Request', () => {
+    expect(drivers(initialState,
+      {
+        type: 'FETCH_DRIVERS_REQUEST'
+      }
+      )).toMatchSnapshot();
+  })
+
+  it ('should handle Fetch Drivers Sucess', () => {
+    expect(drivers(initialState,
+      {
+        type: 'FETCH_DRIVERS_SUCCESS'
+      }
+      )).toMatchSnapshot();
+  })
+
+  it ('should handle Fetch Drivers Failure', () => {
+    expect(drivers(initialState,
+      {
+        type: 'FETCH_DRIVERS_FAILURE'
+      }
+      )).toMatchSnapshot();
+  })
+})
