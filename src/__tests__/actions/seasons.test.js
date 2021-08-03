7d60c885e2a66a8d6fc7e5ff58c5facb60112217
thneves <tsneves11@gmail.com>
2021-08-03 15:41:59 -0300

    Fix css linters

--- src/__tests__/actions/seasons.test.js
+++ src/__tests__/actions/seasons.test.js
@@ -4,39 +4,37 @@ import { fetchSeasonsRequest, fetchSeasonsSuccess, fetchSeasonsFailure } from '.
 const mockStore = configureMockStore();
 const store = mockStore({});
 
-  it('should request the Seasons ranking', () => {
-    store.dispatch(fetchSeasonsRequest(
-      {
-        loading: true,
-        seasons: {},
-        error: '',
-      }),
-    );
-    expect(store.getActions()).toMatchSnapshot();
-  });
+it('should request the Seasons ranking', () => {
+  store.dispatch(fetchSeasonsRequest(
+    {
+      loading: true,
+      seasons: {},
+      error: '',
+    },
+  ));
+  expect(store.getActions()).toMatchSnapshot();
+});
 
-  it('should fetch seasons with success', () => {
-    store.dispatch(
-      fetchSeasonsSuccess({
-        loading: false,
-        seasons: {
-          seasons: [2012, 2013]
-        },
-        error: '',
-      })
-    );
-      expect(store.getActions()).toMatchSnapshot();
-  });
-  
-  
-  it('should fetch seasons with failure', () => {
-    store.dispatch(
-      fetchSeasonsFailure({
-        loading: false,
-        seasons: {},
-        error: 'some error',
-      })
-    );
-    expect(store.getActions()).toMatchSnapshot();
-  });
+it('should fetch seasons with success', () => {
+  store.dispatch(
+    fetchSeasonsSuccess({
+      loading: false,
+      seasons: {
+        seasons: [2012, 2013],
+      },
+      error: '',
+    }),
+  );
+  expect(store.getActions()).toMatchSnapshot();
+});
 
+it('should fetch seasons with failure', () => {
+  store.dispatch(
+    fetchSeasonsFailure({
+      loading: false,
+      seasons: {},
+      error: 'some error',
+    }),
+  );
+  expect(store.getActions()).toMatchSnapshot();
+});
--- src/__tests__/components/checkbox.test.js
+++ src/__tests__/components/checkbox.test.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Checkbox from '../../components/Checkbox/index';
 import * as redux from 'react-redux';
+import Checkbox from '../../components/Checkbox/index';
 
 const useSelectorMock = jest.spyOn(redux, 'useSelector');
 
-beforeEach( () => {
+beforeEach(() => {
   useSelectorMock.mockClear();
-})
+});
 
 it('should render Checkbox component', () => {
-
-  useSelectorMock.mockReturnValue([2012, 2013, 2014])
+  useSelectorMock.mockReturnValue([2012, 2013, 2014]);
   const checkbox = renderer
     .create(
-      <Checkbox checked={() => true} handleChange={() => ''} />
-    )
+      <Checkbox checked={() => true} handleChange={() => ''} />,
+    );
 
-    expect(checkbox.toJSON()).toMatchSnapshot();
-})
+  expect(checkbox.toJSON()).toMatchSnapshot();
+});
